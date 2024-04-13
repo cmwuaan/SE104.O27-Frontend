98d5e2a7c6e1825bc7aeb5a332237acf3deb2ed5
th1enlm02 <21521460@gm.uit.edu.vn>
2024-04-13 14:47:53 +0700

    style: change button bg color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function Button({ title, onClick }) {
   return (
     <button
-      className="w-full bg-primary-color text-white text-[0.7rem] p-[6px] font-medium rounded-sm"
+      className="w-full bg-primary-90 text-white text-[0.7rem] p-[6px] font-medium rounded-sm "
       onClick={onClick}
     >
       {title}
